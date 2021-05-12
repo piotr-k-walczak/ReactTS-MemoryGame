a7c4cbc044d3fff7e00c4a969629a6b109995b1c
Piotr Walczak <piotr.k.walczak@outlook.com>
2021-05-12 17:12:26 +0200

    Added rainbow animation to logo

--- src/Topbar.tsx
+++ src/Topbar.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components"
 import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
@@ -6,6 +7,54 @@ import StyledLink, { StyledButton } from "./StyledText";
 type Props = {
   inGame: boolean;
 };
+
+const StyledTopbar = styled.div`
+  width: 100%;
+  padding: 1.2em 2em 1em;
+  text-align: left;
+  box-sizing: border-box;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.5) 100%);
+  color: white;
+  font-family: 'Josefin Sans', sans-serif;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  justify-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+    align-items: center;
+
+    & > div {
+      margin-top: 4px;
+      margin-bottom: 4px;
+    }
+  }
+`
+
+const StyledLogoLink = styled(StyledLink)`
+  background: -webkit-linear-gradient( 92deg, #95d7e3, #eb76ff );
+  background-size:600vw 600vw;
+
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textAnimate 5s linear infinite alternate;
+  @keyframes textAnimate {
+    from {
+      filter: hue-rotate(0deg);
+      background-position-x: 0%;
+      
+    }
+    to {
+      filter: hue-rotate(360deg);
+      background-position-x: 600vw;
+      
+    }
+  }
+`
 
 function Topbar(props: Props) {
   const history = useHistory();
@@ -22,24 +71,8 @@ function Topbar(props: Props) {
   };
 
   return (
-    <div
-      style={{
-        width: "100%",
-        padding: "1.2em 2em 1em",
-        textAlign: "left",
-        boxSizing: "border-box",
-        background:
-          "linear-gradient(0deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.5) 100%)",
-        color: "white",
-        fontFamily: "'Josefin Sans', sans-serif",
-        display: "flex",
-        justifyContent: "space-between",
-        position: "sticky",
-        top: 0,
-        zIndex: 100,
-      }}
-    >
-      <StyledLink to="/">Memory Game</StyledLink>
+    <StyledTopbar>
+      <StyledLogoLink to="/" style={{fontWeight:'bold', fontSize:'1.1em'}}>Memory Game</StyledLogoLink>
       {props.inGame && (
         <>
           <div>
@@ -53,7 +86,7 @@ function Topbar(props: Props) {
           </div>
         </>
       )}
-    </div>
+    </StyledTopbar>
   );
 }
 
